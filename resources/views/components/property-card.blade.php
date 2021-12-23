304bcbf8c90c3b0dd185acef853bfd21a8cf0633
soulLess-Coder <52181659+soulLess-Coder@users.noreply.github.com>
2021-12-23 17:06:04 +0500

    Fixed Tenant Listing Page Route

--- resources/views/components/property-card.blade.php
+++ resources/views/components/property-card.blade.php
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
-<div class="bg-white border-2 hover:border-red-500 max-w-sm rounded overflow-hidden shadow-lg p 4">
-  <img class="w-full p-4 " src="/images/red-house.png" alt="Sunset in the mountains">
-  <div class="px-4 py-2">
-    <div class="font-bold text-xl mb-2">St,24 ,Chamanabad</div>
-    <div class = "px-6 pt-4 pb-2">
-        <span ><img class="inline h-6 w-8" src="./images/bed.png ">  </span>
-        <span ><img class=" inline h-6 w-8" src="./images/area.png ">  </span>
-        <span><img class=" inline h-6 w-8" src="./images/bath.png ">  </span>
-        <span ><img class="inline h-6 w-8" src="./images/money.png ">  </span>
-        <span ><img class="inline h-6 w-8" src="./images/location.png ">  </span>
-        <span ><img class="inline h-6 w-8" src="./images/calender.png ">  </span>            
-    </div>
-    
-</div>
-=======
-<div>
-    <div>
-        <h3>
+<div id="card" class="bg-white border-2 hover:border-red-300 max-w-sm rounded overflow-hidden shadow-lg p 4">
+    <img class="w-full p-4 " src="/images/red-house.png" alt="Sunset in the mountains">
+    <div class="px-4 py-2">
 
-        </h3>
+    <div id="cardTitle" class="font-bold text-xl mb-2">St,24,Chamanabad</div>
+
+    <div class = "flex items-center px-6 pt-4 pb-2">
+        <span><img class="inline h-6 w-8" src="./images/bed.png">  </span>
+        <span><img class="inline h-6 w-8" src="./images/area.png">  </span>
+        <span><img class="inline h-6 w-8" src="./images/bath.png">  </span>
+        <span><img class="inline h-6 w-8" src="./images/money.png">  </span>
+        <span><img class="inline h-6 w-8" src="./images/location.png"> </span>
+        <span><img class="inline h-6 w-8" src="./images/calendar.png"> </span>
     </div>
 </div>
->>>>>>> 40e41692687e1ec881cb2583c14038e3a2aaddeb
