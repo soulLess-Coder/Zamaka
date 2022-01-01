8e53f8813cd22ed92699c7e25a99aa5af4b57fab
uaamer27 <usmanaamer919@gmail.com>
2022-01-01 19:49:52 +0500

    login done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,34 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-<form action="{{ route('login') }}" method="post" class="grid grid-cols-1 bg-white sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
-    @csrf
-    <div class="hidden lg:block">
-        <div class="justify-center flex"><a class="mx-auto"><img src="./images/lock1.png" /></a></div>
-    </div>
-    <div class="text-center text-left mix-blend-multiply bg-green-300 lg:bg-white lg:from-white lg:to-white md:bg-white md:from-white md:to-white ">
-      <div class="flex justify-center">
-        <div class="rounded-lg">
-          <p class="text-center text-2xl"><b>Sign In</b></p>
-          <br />
-          <div class="mb-4">
-            <label for="email" class="text-center block font-bold">Email</label>
-            <input type="email" name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-full @error('email') border-red-500 @enderror" value="{{ old('email') }}" />
-          </div>
-          <br />
-          <div class="mb-4">
-            <label for="password" class="text-center block font-bold">Password</label>
-            <input type="password" name="password" id="password" placeholder="Your password" class="bg-gray-100 border-2 w-full h-14 p-4 rounded-full @error('password') border-red-500 @enderror" value="" />
-          </div>
-          <br />
-          <x-inputs.button type="submit" class=" mx-auto" buttonText="Login" />
-          <label class="text-center block">_____________OR____________</label>
-          <div class="justify-center flex"><a class="mx-auto"><img src="./images/googleicon.png" /></a></div>
-          <p class="text-center">Don't have an account? <a href="register.blade.php" class=" text-blue-600">Register</a></p>
-        </div>
+<div class="sm:flex justify-center items-center sm:h-screen">
+    <div class="grid grid-cols-1 m-10 sm:grid-cols-2 ">
+      <div class="sm:col-span-2 flex justify-center">
+        <h1 class="text-5xl leading-loose text-gray-500 flex pt-16">Sign In</h1>
+        <br>
       </div>
+
+      <div class="row-span-6 sm:bg-white w-auto h-72">
+        <img src="./images/lock1.png" />
+      </div>
+
+      <div class="">
+        <label class="pt-16 order-4 sm:flex justify-center">Email</label>
+        <input type="text" id="Street" placeholder="@" class="w-full h-1 p-4 mt-2 transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
+      </div>
+
+      <div class="">
+        <label class="pt-5 sm:flex justify-center">Password</label>
+        <input type="text" id="Street" placeholder="@" class="flex justify-center w-full h-1 p-4 mt-2 transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
+      </div>
+
+      <div class=" w-3/4 h-10 shadow-xl m-10">
+        <x-inputs.button type="submit" class=" mx-auto" buttonText="Login" />
+      </div>
+
+      <hr />
+
+      <div class="justify-center flex"><a class="mx-auto"><img src="./images/googleicon.png" /></a></div>
+
+      <p class="text-center">Don't have an account? <a href="register.blade.php" class=" text-blue-600">Register</a></p>
     </div>
-  </form>
 </div>
 @endsection
