d002b5363de1e93d8c47660b4b3a85126f4a04a6
rajamahad96 <rajamahad96@gmail.com>
2022-01-27 12:00:28 +0500

    compliants page

--- resources/views/tenant/complaints.blade.php
+++ resources/views/tenant/complaints.blade.php
@@ -9,23 +9,20 @@
         <p><b>You complaint thought complaint portal we are always there to help</b></p>
       </div>
       <div>
-        <label class="justify-center p-2  sm:flex">Name</label>
+        <label class="justify-center p-2 sm:flex">Name</label>
         <input type="text" id="Street" placeholder="Raja Muhammad Mahad" class="flex justify-center w-full h-1 p-4 transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
       </div>
 
-       <div>
-        <label class="justify-center p-2  sm:flex">Address</label>
-        <input type="text" id="Street" placeholder="h#11, St#4,Chamanabad dhoke Sydean Rawalpindi" class="flex justify-center w-full h-1 p-4 transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100">
-      </div>
+
 
        <div>
-        <label class="justify-center p-2  sm:flex">Number</label>
+        <label class="justify-center p-2 sm:flex">Number</label>
         <input type="number" id="Street" placeholder="03xx-xxxxxxx" class="flex justify-center w-full h-1 p-4 transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
       </div>
 
        <div>
-        <label class="justify-center p-2  sm:flex">Details</label>
-        <input type="text" id="Street" placeholder="" class="flex justify-center w-full h-1 p-4 transform bg-gray-200 border-2 shadow-lg resize  r hover:translate-x-1 hover:border-gray-100" />
+        <label class="justify-center p-2 sm:flex">Details</label>
+        <input type="text" id="Street" placeholder="" class="flex justify-center w-full h-1 p-4 transform bg-gray-200 border-2 shadow-lg resize r hover:translate-x-1 hover:border-gray-100" />
       </div>
 
       <div>
