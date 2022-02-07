8cd492260586847a40a5973412c172e11f240b96
rajamahad96 <rajamahad96@gmail.com>
2022-02-07 21:48:56 +0500

    register pages changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,39 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="sm:flex justify-center items-center sm:h-screen">
+
+<div class="items-center justify-center sm:flex sm:h-screen">
+
     <div class="grid grid-cols-1 m-10 sm:grid-cols-2 ">
-      <div class="sm:col-span-2 flex justify-center">
-        <h1 class="text-5xl leading-loose flex pt-16">Sign Up</h1>
+      <div class="flex justify-center sm:col-span-2">
+        <h1 class="flex pt-16 text-5xl leading-loose">Sign Up</h1>
         <br>
       </div>
-      <div class="row-span-6 sm:bg-white w-auto h-72 p-5">
+      <div class="w-auto row-span-6 p-5 sm:bg-white h-72">
         <img src="./images/signup.png" />
       </div>
 
-      <div class="w-full bg-gray-200 rounded-full text-center font-bold ">
+      <div class="w-full font-bold text-center bg-gray-200 rounded-full ">
         0%
       </div>
-      <div class="lg: pt-6 ">
-        <label class=" order-4 sm:flex justify-center font-bold ">Full Name</label>
+      <div class="pt-6 lg: ">
+        <label class="justify-center order-4 font-bold  sm:flex">Full Name</label>
         <input type="text" id="Street" placeholder="@" class="w-full h-1 p-4 mt-2 transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
       </div>
 
-      <div class="lg: pt-6">
-        <label class="sm:flex justify-center font-bold pt-4 ">Phone No</label>
+      <div class="pt-6 lg:">
+        <label class="justify-center pt-4 font-bold sm:flex ">Phone No</label>
         <input type="text" id="Street" placeholder="@" class="flex justify-center w-full h-1 p-4 mt-2 transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
       </div>
-      <div class="lg: pt-6">
-        <label class="sm:flex justify-center font-bold pt-4 ">CNIC</label>
+      <div class="pt-6 lg:">
+        <label class="justify-center pt-4 font-bold sm:flex ">CNIC</label>
         <input type="text" id="Street" placeholder="@" class="flex justify-center w-full h-1 p-4 mt-2 transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
       </div>
-      <div class="inline-block pl-4 pr-4 pt-5">
+      <div class="inline-block pt-5 pl-4 pr-4">
                 <input class="" type="checkbox" checked >
                 <label>By clicking this I Agree term and condition</label>
       </div>
-        <div class=" w-3/4 h-10 shadow-xl m-10">
-        <x-inputs.button type="submit" class=" mx-auto" buttonText="Next" />
-        <p>Already have an account? <a href="login.blade.php" class=" text-blue-600">Login</a></p>
+        <div class="w-3/4 h-10 m-10 shadow-xl ">
+        <x-inputs.button type="submit" class="mx-auto " buttonText="Next" />
+        <p>Already have an account? <a href="login.blade.php" class="text-blue-600 ">Login</a></p>
       </div>
 
     </div>
