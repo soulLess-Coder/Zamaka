a00f5e0d27a59fdf12c4ec31355e005273516731
rajamahad96 <rajamahad96@gmail.com>
2022-02-24 16:02:24 +0500

    reset ,forget page

---
+++ resources/views/auth/resetpass.blade.php
@@ -0,0 +1,39 @@
+
+
+<div class="flex flex-col items-center p-3 mx-3   h-screen rounded-2xl">
+    <div class=" ">
+      <h2 class="p-10 text-4xl font-bold p-auto">Reset Password?</h2>
+    </div>
+
+
+    <div class="flex justify-center pt-10 font-bold">
+      <h3>Current Password</h3>
+    </div>
+
+    <div class="flex items-center justify-center w-96 p-5">
+      <input type="password" id="Street" placeholder="Current password" class="text-center w-full h-10 p-5 transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
+    </div>
+
+    <!-- New Password -->
+
+      <div class="flex justify-center pt-2 font-bold">
+      <h3>New Password</h3>
+    </div>
+        <div class="flex items-center justify-center w-96 p-5">
+      <input type="password" id="Street" placeholder="New Password" class="text-center w-full h-10 p-5 transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
+    </div>
+    <div class="flex items-center justify-center w-96 p-5">
+      <input type="password" id="Street" placeholder="Re-Type New Password" class="text-center w-full h-10 p-5 transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
+    </div>
+    <div class="flex flex-wrap pl-5 justify justify-center md:justify-center">
+      <h3 class="flex flex-wrap text-red-600">Sorry! password did'nt match  TryAgain.</h3>
+    </div>
+
+    <div class="flex flex-row-reverse p-5 transform hover:translate-x-2">
+      <button class="w-20 bg-red-500 rounded-full h-7">Submit</button>
+    </div>
+
+    <div class="flex justify-center md:justify-center flex-wrap">
+      <h3>Already have an Account? <u class="text-blue-600">Login</u></h3>
+    </div>
+</div>
