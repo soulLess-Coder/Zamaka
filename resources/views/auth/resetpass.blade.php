f2d7c0dc966c0a4cc994224d512bfbd4c918397d
rajamahad96 <rajamahad96@gmail.com>
2022-02-24 16:04:55 +0500

    reset forget pass

--- resources/views/auth/resetpass.blade.php
+++ resources/views/auth/resetpass.blade.php
@@ -1,7 +1,7 @@
 
 
-<div class="flex flex-col items-center p-3 mx-3   h-screen rounded-2xl">
-    <div class=" ">
+<div class="flex flex-col items-center h-screen p-3 mx-3 rounded-2xl">
+    <div class="">
       <h2 class="p-10 text-4xl font-bold p-auto">Reset Password?</h2>
     </div>
 
@@ -10,8 +10,8 @@
       <h3>Current Password</h3>
     </div>
 
-    <div class="flex items-center justify-center w-96 p-5">
-      <input type="password" id="Street" placeholder="Current password" class="text-center w-full h-10 p-5 transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
+    <div class="flex items-center justify-center p-5 w-96">
+      <input type="password" id="Street" placeholder="Current password" class="w-full h-10 p-5 text-center transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
     </div>
 
     <!-- New Password -->
@@ -19,13 +19,13 @@
       <div class="flex justify-center pt-2 font-bold">
       <h3>New Password</h3>
     </div>
-        <div class="flex items-center justify-center w-96 p-5">
-      <input type="password" id="Street" placeholder="New Password" class="text-center w-full h-10 p-5 transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
+        <div class="flex items-center justify-center p-5 w-96">
+      <input type="password" id="Street" placeholder="New Password" class="w-full h-10 p-5 text-center transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
     </div>
-    <div class="flex items-center justify-center w-96 p-5">
-      <input type="password" id="Street" placeholder="Re-Type New Password" class="text-center w-full h-10 p-5 transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
+    <div class="flex items-center justify-center p-5 w-96">
+      <input type="password" id="Street" placeholder="Re-Type New Password" class="w-full h-10 p-5 text-center transform bg-gray-200 border-2 rounded-full shadow-lg hover:translate-x-1 hover:border-gray-100" />
     </div>
-    <div class="flex flex-wrap pl-5 justify justify-center md:justify-center">
+    <div class="flex flex-wrap justify-center pl-5 justify md:justify-center">
       <h3 class="flex flex-wrap text-red-600">Sorry! password did'nt match  TryAgain.</h3>
     </div>
 
@@ -33,7 +33,7 @@
       <button class="w-20 bg-red-500 rounded-full h-7">Submit</button>
     </div>
 
-    <div class="flex justify-center md:justify-center flex-wrap">
+    <div class="flex flex-wrap justify-center md:justify-center">
       <h3>Already have an Account? <u class="text-blue-600">Login</u></h3>
     </div>
 </div>
