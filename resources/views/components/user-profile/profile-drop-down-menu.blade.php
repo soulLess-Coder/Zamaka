f5b8d82714e26aa594aaf0000680b0684a255124
SoulLess-Coder <phantomtiger04@gmail>
2022-02-25 03:02:53 +0500

    sfgh

--- resources/views/components/user-profile/profile-drop-down-menu.blade.php
+++ resources/views/components/user-profile/profile-drop-down-menu.blade.php
@@ -7,7 +7,7 @@
     <!-- end item -->
 
     <!-- item -->
-    <a class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900" href="{{ route('forgetpassword') }}">
+    <a class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900" href="#">
         <i class="mr-1 text-xs fad fa-inbox-in"></i>
         Change Password
     </a>
