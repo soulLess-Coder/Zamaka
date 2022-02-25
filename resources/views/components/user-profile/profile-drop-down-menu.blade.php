342304ec283afb6b3741abbb0d3d34e944d40725
rajamahad96 <rajamahad96@gmail.com>
2022-02-25 10:43:23 +0500

    rest paass kink

--- resources/views/components/user-profile/profile-drop-down-menu.blade.php
+++ resources/views/components/user-profile/profile-drop-down-menu.blade.php
@@ -1,4 +1,4 @@
-<div class="absolute right-0 z-20 w-40 py-2 mt-3 text-gray-500 bg-white rounded shadow-md menu md:mt-6 md:w-1/3 border-2 animated faster fadeIn hidden" id="drop-down">
+<div class="absolute right-0 z-20 hidden w-40 py-2 mt-3 text-gray-500 bg-white border-2 rounded shadow-md menu md:mt-6 md:w-1/3 animated faster fadeIn" id="drop-down">
     <!-- item -->
     <a class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900" href="{{ route('userprofile') }}">
         <i class="mr-1 text-xs fad fa-user-edit"></i>
@@ -7,7 +7,7 @@
     <!-- end item -->
 
     <!-- item -->
-    <a class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900" href="#">
+    <a class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900" href="">
         <i class="mr-1 text-xs fad fa-inbox-in"></i>
         Change Password
     </a>
