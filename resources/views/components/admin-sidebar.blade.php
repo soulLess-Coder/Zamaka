702df9b4108318f368ef1b351e6f9d1031b32fb4
rajamahad96 <rajamahad96@gmail.com>
2022-02-26 15:21:32 +0500

    admin page changes

--- resources/views/components/admin-sidebar.blade.php
+++ resources/views/components/admin-sidebar.blade.php
@@ -27,7 +27,7 @@
                             <path stroke="none" d="M0 0h24v24H0z"></path>
                             <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1"></path>
                         </svg>
-                        <span class="text-sm ml-2">Products</span>
+                        <span class="text-sm ml-2">Agent Request</span>
                     </a>
                     <div class="py-1 px-3 bg-gray-600 rounded text-gray-300 flex items-center justify-center text-xs">8</div>
                 </li>
@@ -38,7 +38,7 @@
                             <polyline points="8 16 10 10 16 8 14 14 8 16"></polyline>
                             <circle cx="12" cy="12" r="9"></circle>
                         </svg>
-                        <span class="text-sm ml-2">Performance</span>
+                        <span class="text-sm ml-2">Auth Property</span>
                     </a>
                 </li>
                 <li class="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
@@ -49,7 +49,7 @@
                             <polyline points="17 8 21 12 17 16"></polyline>
                             <line x1="14" y1="4" x2="10" y2="20"></line>
                         </svg>
-                        <span class="text-sm ml-2">Deliverables</span>
+                        <span class="text-sm ml-2">Assigning Task</span>
                     </a>
                 </li>
                 <li class="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
@@ -58,7 +58,7 @@
                             <path stroke="none" d="M0 0h24v24H0z"></path>
                             <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1"></path>
                         </svg>
-                        <span class="text-sm ml-2">Invoices</span>
+                        <span class="text-sm ml-2">Roles</span>
                     </a>
                     <div class="py-1 px-3 bg-gray-600 rounded text-gray-300 flex items-center justify-center text-xs">25</div>
                 </li>
@@ -70,7 +70,17 @@
                             <polyline points="4 12 12 16 20 12" />
                             <polyline points="4 16 12 20 20 16" />
                         </svg>
-                        <span class="text-sm ml-2">Inventory</span>
+                        <span class="text-sm ml-2">Maintance record</span>
+                    </a>
+                </li>
+                <li class="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center">
+                    <a href="javascript:void(0)" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-settings" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                            <circle cx="12" cy="12" r="3" />
+                        </svg>
+                        <span class="text-sm ml-2">Settings</span>
                     </a>
                 </li>
                 <li class="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center">
