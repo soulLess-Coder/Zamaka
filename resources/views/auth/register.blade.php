bfd4a34122924694b5549afdac2abd59740854ba
rajamahad96 <rajamahad96@gmail.com>
2022-03-01 10:06:19 +0500

    hello

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,7 +17,7 @@
             <div class="" >
                 <div class="justify-center pt-6">
                     <label for="name" class="justify-center order-4 font-bold sm:flex">Full Name</label>
-                    <input type="text" name="name" id="name" placeholder="Your Full Name" class="w-1/2 h-5 p-4 mt-2 ml-10 text-red-200 transform border-2 rounded-lg shadow-md  bg-neutral-400 hover:translate-x-1 hover:border-gray-100" />
+                    <input type="text" name="name" id="name" placeholder="Your Full Name" class="w-1/2 h-5 p-4 mt-2 ml-10 text-gray-900 transform border-2 rounded-lg shadow-md  bg-neutral-400 hover:translate-x-1 hover:border-gray-100" />
                     @error('name')
                     <div class="mt-2 text-sm text-red-500">
                         {{ $message }}
@@ -28,7 +28,7 @@
 
                 <div class="pt-6">
                     <label for="password" class="flex items-start pt-4 font-bold sm:flex ">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Choose a password" class="w-1/2 h-5 p-4 mt-2 ml-10 text-red-300 transform border-2 rounded-lg shadow-md  bg-neutral-400 hover:translate-x-1 hover:border-gray-100" />
+                    <input type="password" name="password" id="password" placeholder="Choose a password" class="w-1/2 h-5 p-4 mt-2 ml-10 text-gray-900 transform border-2 rounded-lg shadow-md  bg-neutral-400 hover:translate-x-1 hover:border-gray-100" />
                     @error('password')
                         <div class="mt-2 text-sm text-red-500">
                             {{ $message }}
@@ -38,7 +38,7 @@
 
                 <div class="pt-6">
                     <label for="password_confirmation" class="justify-center pt-4 font-bold sm:flex ">Confirm password</label>
-                    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm your password" class="flex w-1/2 h-5 p-4 mt-2 ml-10 text-red-300 transform border-2 shadow-lg rounded-xl bg-neutral-400 hover:translate-x-1 hover:border-gray-100" />
+                    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm your password" class="flex w-1/2 h-5 p-4 mt-2 ml-10 text-gray-900 transform border-2 shadow-lg rounded-xl bg-neutral-400 hover:translate-x-1 hover:border-gray-100" />
                     @error('password_confirmation')
                         <div class="mt-2 text-sm text-red-500">
                             {{ $message }}
@@ -48,7 +48,7 @@
 
                 <div class="pt-6">
                     <label for="email" class="justify-center pt-4 font-bold sm:flex ">Email</label>
-                    <input type="email" name="email" id="email" placeholder="Your Email" class="justify-center w-1/2 h-3 p-4 mt-2 ml-10 text-red-300 transform border-2 shadow-lg rounded-xl bg-neutral-400 hover:translate-x-1 hover:border-gray-100" />
+                    <input type="email" name="email" id="email" placeholder="Your Email" class="justify-center w-1/2 h-3 p-4 mt-2 ml-10 text-gray-900 transform border-2 shadow-lg rounded-xl bg-neutral-400 hover:translate-x-1 hover:border-gray-100" />
                     @error('email')
                         <div class="mt-2 text-sm text-red-500">
                             {{ $message }}
@@ -57,7 +57,7 @@
                 </div>
                 <div class="pt-6">
                     <label for="phone_number" class="justify-center pt-4 font-bold sm:flex ">Phone Number</label>
-                    <input type="text" name="phone_number" id="phone_number" placeholder="03-xxx-xxxx-xx" class="justify-center w-1/2 h-3 p-4 mt-2 ml-10 text-red-300 transform border-2 shadow-lg rounded-xl bg-neutral-400 hover:translate-x-1 hover:border-gray-100" />
+                    <input type="text" name="phone_number" id="phone_number" placeholder="03-xxx-xxxx-xx" class="justify-center w-1/2 h-3 p-4 mt-2 ml-10 text-gray-900 transform border-2 shadow-lg rounded-xl bg-neutral-400 hover:translate-x-1 hover:border-gray-100" />
                     @error('email')
                         <div class="mt-2 text-sm text-red-500">
                             {{ $message }}
--- resources/views/components/user-profile/admin-profile-dropdown.blade.php
+++ resources/views/components/user-profile/admin-profile-dropdown.blade.php
@@ -11,10 +11,6 @@
         <i class="mr-1 text-xs fad fa-inbox-in"></i>
         Change Password
     </a>
-    <a class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900" href="{{ route('/tenant/request') }}">
-        <i class="mr-1 text-xs fad fa-inbox-in"></i>
-        House Detail
-    </a>
     <!-- end item -->
     <hr>
 
