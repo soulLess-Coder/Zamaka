ef866fc9608a69335e788367119f104cb911ec60
basitbaig1122 <basiitbaig@gmail.com>
2022-03-01 09:34:07 +0500

    Fixed Profile

--- resources/views/components/user-profile/profile-drop-down-menu.blade.php
+++ resources/views/components/user-profile/profile-drop-down-menu.blade.php
@@ -13,7 +13,13 @@
     </a>
     <!-- end item -->
     <hr>
-
+    @if(Auth::user()->role=="Tenant"){
+        <a class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900" href="{{ route('resetpass') }}">
+        <i class="mr-1 text-xs fad fa-inbox-in"></i>
+            Tenant Dashboard
+        </a>
+    }
+    @endif
     <a class="block px-4 py-2 text-sm font-medium tracking-wide capitalize transition-all duration-300 ease-in-out bg-white hover:bg-gray-200 hover:text-gray-900" href="/tenant/maintenance_request">
         <i class="mr-1 text-xs fad fa-user-edit"></i>
         Maintanance
