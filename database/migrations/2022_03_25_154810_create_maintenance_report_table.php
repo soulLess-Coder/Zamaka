298cc0a012bb6591317285dacbf99b3d3e6b7088
SoulLess-Coder <phantomtiger04@gmail>
2022-03-30 12:09:00 +0500

    UpdaTED

---
+++ database/migrations/2022_03_25_154810_create_maintenance_report_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMaintenanceReportTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maintenance_report', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('property_id');
+            $table->foreign('property_id')->references('id')->on('properties');
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->string('category');
+            $table->string('image');
+            $table->string('video');
+            $table->decimal('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('maintenance_report');
+    }
+}
