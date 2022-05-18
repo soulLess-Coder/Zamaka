3a5bcebcfb60c1cfaffd7f50e32676554f7a6422
SoulLess-Coder <phantomtiger04@gmail>
2022-05-19 01:32:11 +0500

    Trying to fix request

--- app/Http/Controllers/View/LoginController.php
+++ app/Http/Controllers/View/LoginController.php
@@ -12,15 +12,24 @@ class LoginController extends Controller
         return view('auth.login');
     }
 
-    public function login()
+    public function login(Request $request)
     {
-        $client = new Client();
-//        $request = $client->createRequest('POST', 'http://localhost:8000/api/auth/login');
-//        $response = $client->send($request);
-        $response = $client->post('http://localhost:8000/api/auth/login');
-//        $request = Request::create('', 'POST');
-//        $response = app()->handle($request);
-        $user = $response->json();
+//        $client = new Client();
+//        $r = $client->request('POST', 'http://localhost:8000/api/auth/login', [
+//            'form_params' => [
+//                'email' => $request->email,
+//                'password' => $request->password,
+//            ]
+//        ]);
+//        $response = $client->send($r);
+        //$response = $client->post('http://localhost:8000/api/auth/login');
+
+        $r = Request::create('http://localhost:8000/api/auth/login', 'POST',[
+               'email' => $request->email,
+               'password' => $request->password,
+        ]);
+        $response = app()->handle($r);
+        $user = $response->getContent();
         $code = $response->getStatusCode();
         //$role = Request::parseHeader($request, 'role');
         dd($user);
